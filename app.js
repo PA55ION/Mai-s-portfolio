218a035d3827a7c00e1c6ac206a24de0be98f968
Mai Campbell <jirallc@gmail.com>
2020-05-13 21:02:50 -0400

    refactor: clean up code

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 window.addEventListener("load", function() {
     let load_screen = document.getElementById('load_screen');
     document.body.removeChild(load_screen);
-});
+}, 5000);
 
+const links = document.querySelectorAll('.links');
+const sections = document.querySelectorAll('section');
+
+
+function changeLinkState() {
+    let index = sections.length;
+
+    while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  
+    links.forEach((link) => link.classList.remove('active'));
+    links[index].classList.add('active');
+}
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
